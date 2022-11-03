4ff177041e36e2cc2826eb442033bc5a2617b125
ospodaryk <ospodaryk11@gmail.com>
2022-11-03 21:13:28 +0200

    Make first screen git.Add resourcemanager for door and cash. Add icons for queue

---
+++ RailwayStation/src/main/java/com/example/railwaystation/classes/Rendering/ResourceManagerDoor.java
@@ -0,0 +1,16 @@
+package com.example.railwaystation.classes.Rendering;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceManagerDoor {
+    private static final Map<Integer, Image> sprites = new HashMap<>();
+    public Image getSprite(Integer key){
+        return sprites.get(key);
+    }
+    public void setSprite(Integer key, Image sprite){
+        sprites.put(key, sprite);
+    }
+}
